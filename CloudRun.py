8448fa313b170736606a862dedc910c28bd90fbf
SkiCzr <vintea.nicu.cezar@gmail.com>
2025-01-24 14:16:05 +0100

    Added flask app running for continuous running

--- CloudRun.py
+++ CloudRun.py
@@ -1,6 +1,21 @@
 from flask import Flask
-
+from threading import Thread
 from FileAutoBuyer import start_from_file
+
 app = Flask(__name__)
-app.run(host="0.0.0.0", port=5000)
-start_from_file()
+
+
+# Define a function to run the background task
+def run_background_task():
+    start_from_file()
+
+
+# Start the Flask app
+if __name__ == "__main__":
+    # Start the background task in a separate thread
+    background_thread = Thread(target=run_background_task)
+    background_thread.daemon = True  # Ensures the thread will close when the main program exits
+    background_thread.start()
+
+    # Run the Flask app
+    app.run(host="0.0.0.0", port=5000)
