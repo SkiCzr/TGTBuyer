603ce8c2e5257a1b41fe822fd736ded21c3402d7
SkiCzr <vintea.nicu.cezar@gmail.com>
2025-01-24 14:09:24 +0100

    Corrected custom stops calculations

--- CloudRun.py
+++ CloudRun.py
@@ -1,3 +1,6 @@
-from FileAutoBuyer import start_from_file
+from flask import Flask
 
+from FileAutoBuyer import start_from_file
+app = Flask(__name__)
+app.run(host="0.0.0.0", port=5000)
 start_from_file()
